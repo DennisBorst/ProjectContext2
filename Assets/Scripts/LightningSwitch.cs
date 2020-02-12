fefeacb25dc34449204e07904fce818847de0bca
DennisBorst <dennis.borst@hotmail.nl>
2020-02-12 14:50:50 +0100

    Lightning switch added (you can find it in the prefabs folder)

---
+++ Assets/Scripts/LightningSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningSwitch : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+    [Header("Rotation")]
+    [SerializeField] private float rotationChangeSpeed;
+    [SerializeField] private Vector3 rotationLight;
+    [Header("Color")]
+    [SerializeField] private float colorChangeSpeed;
+    [SerializeField] private Color32 colorLight;
+    [Header("Light intensity")]
+    [SerializeField] private float intensityChangeSpeed;
+    [SerializeField] private float newIntensity;
+
+    private bool switchLight = false;
+    private Vector3 currentRotation;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            switchLight = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (switchLight)
+        {
+            SwitchLightningSettings();
+        }
+    }
+
+    private void SwitchLightningSettings()
+    {
+        Quaternion rotation = Quaternion.Euler(rotationLight);
+        directionalLight.transform.rotation = Quaternion.Lerp(directionalLight.transform.rotation, rotation, rotationChangeSpeed);
+
+        directionalLight.color = Color32.Lerp(directionalLight.color, colorLight, colorChangeSpeed);
+        directionalLight.intensity = Mathf.Lerp(directionalLight.intensity, newIntensity, intensityChangeSpeed);
+    }
+}
--- Assets/Scripts/MiddlePointPlayers.cs
+++ Assets/Scripts/MiddlePointPlayers.cs
@@ -8,7 +8,16 @@ public class MiddlePointPlayers : MonoBehaviour
     [SerializeField] private List<Player> players;
 
     private float distancePlayer;
+    private Vector3 centerPoint;
+    //private List<Vector3> previousPosition = new List<Vector3>();
 
+    private void Start()
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            //previousPosition.Add(players[i].transform.position);
+        }
+    }
     private void Update()
     {
         distancePlayer = Vector3.Distance(players[0].transform.position, players[1].transform.position);
@@ -21,11 +30,44 @@ public class MiddlePointPlayers : MonoBehaviour
         if(distancePlayer >= walkingDistance)
         {
             //InvisibleWall
+            Debug.Log("Distance between players is to big");
+
+            CheckPlayerPosition();
+        }
+        else
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].ResetCharacter(players[i].movementSpeed);
+            }
+        }
+    }
+
+    private void CheckPlayerPosition()
+    {
+        if(players[0].inputX > 0.5f && -0.5 > players[1].inputX || players[1].inputX > 0.5f && -0.5 > players[0].inputX ||
+           players[0].inputZ > 0.5f && -0.5 > players[1].inputZ || players[1].inputZ > 0.5f && -0.5 > players[0].inputZ)
+        {
+            players[0].ResetCharacter(0);
+            players[1].ResetCharacter(0);
+
+            players[0].transform.position = Vector3.MoveTowards(players[0].transform.position, centerPoint, 0.1f);
+            players[1].transform.position = Vector3.MoveTowards(players[1].transform.position, centerPoint, 0.1f);
+        }
+        else if(players[0].inputX > 0.5f || players[0].inputX < -0.5f || players[0].inputZ > 0.5f || players[0].inputZ < -0.5f)
+        {
+            players[0].ResetCharacter(0);
+            players[0].transform.position = Vector3.MoveTowards(players[0].transform.position, centerPoint, 0.1f);
+        }
+        else if(players[1].inputX > 0.5f || players[1].inputX < -0.5f || players[1].inputZ > 0.5f || players[1].inputZ < -0.5f)
+        {
+            players[1].ResetCharacter(0);
+            players[1].transform.position = Vector3.MoveTowards(players[1].transform.position, centerPoint, 0.1f);
         }
     }
     private void LateUpdate()
     {
-        Vector3 centerPoint = GetCenterPoint();
+        centerPoint = GetCenterPoint();
 
         transform.position = centerPoint;
     }
