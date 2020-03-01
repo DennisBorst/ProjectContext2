2b89dcc4032659a094358576762138412a81ebb1
DennisBorst <dennis.borst@hotmail.nl>
2020-03-01 02:38:39 +0100

    chapter 2 updated, character input improved, cinematic 3 added

--- Assets/Scripts/CameraRelated/CameraDataCollecter.cs
+++ Assets/Scripts/CameraRelated/CameraDataCollecter.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraDataCollecter : MonoBehaviour
 {
     [SerializeField] private float yAs;
+    //[SerializeField] private CinemachineBrain cinemachineBrain;
+    //private Transform cam;
 
     private void Update()
     {
-        yAs = transform.rotation.y;
+        yAs = transform.eulerAngles.y;
+
+        /*
+        if(yAs > 120)
+        {
+            yAs = transform.rotation.y * 160;
+        }
+        else
+        {
+            yAs = transform.rotation.y * 130;
+        }
+        */
+        //yAs = cam.rotation.y;
+        //yAs = m_WorldUpOverride.rotation.y * 130;
+        ///yAs = cinemachineBrain.transform.rotation.y;
     }
 }
