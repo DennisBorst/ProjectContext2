7f42c6d41766b6e75f98341af0791ffb94577c01
DennisBorst <dennis.borst@hotmail.nl>
2020-03-03 12:57:06 +0100

    Chapter 1 updated

---
+++ Assets/Scripts/ActivateInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateInteraction : Interactable
+{
+    [SerializeField] private GameObject setObjectActive;
+
+    [SerializeField] private bool manCanActivate;
+    [SerializeField] private bool womanCanActivate;
+
+    private void Update()
+    {
+        if (manCanActivate)
+        {
+            if (manCollding)
+            {
+                if (man.interact)
+                {
+                    setObjectActive.SetActive(true);
+                }
+            }
+        }
+        else if (womanCanActivate)
+        {
+            if (womanCollding)
+            {
+                if (woman.interact)
+                {
+                    setObjectActive.SetActive(true);
+                }
+            }
+        }
+    }
+}
