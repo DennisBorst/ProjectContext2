eb27b59da51efbdc2a48f2ace0f7af3d35c5dc21
DennisBorst <dennis.borst@hotmail.nl>
2020-03-22 17:44:50 +0100

    Hyena added

---
+++ Assets/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelTrigger : Interactable
+{
+    void Update()
+    {
+        if(manCollding && womanCollding)
+        {
+            GameManager.Instance.LoadNextLevel();
+        }
+    }
+}
